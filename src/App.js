5b4cc8fe90f79b1c21ef0c051c5680b6d9e2ad1b
mozolevsky <alf.rulit@gmail.com>
2018-03-12 19:14:24 +0300

    Static webpack configs, added CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <h1 className={classes.AppTitle}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={classes.AppIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
